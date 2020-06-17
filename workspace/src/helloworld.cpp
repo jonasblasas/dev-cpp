82f7b5c58b05f2df5ded6d8f115d11e5b528c411
Jonas @ CoopX <jonas.blasas.lonnum@coop.no>
2020-06-17 15:46:34 +0200

    Initial commit

---
+++ workspace/src/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    while (true)
+    {
+        continue;
+    }
+    
+    std::cout << "Hello world program exited!" << std::endl;
+    return 0;
+}
